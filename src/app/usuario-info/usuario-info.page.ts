16101825c58dd04e867ee785702f9e58c9fbc3e5
tarre98 <juanete-1998@hotmail.es>
2021-06-01 14:35:53 +0200

    pagina usuario

--- src/app/usuario-info/usuario-info.page.ts
+++ src/app/usuario-info/usuario-info.page.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { IUser, IUser2 } from '../Interfaces';
+import { LibroService } from '../services/Libros.service';
+
 
 @Component({
   selector: 'app-usuario-info',
   templateUrl: './usuario-info.page.html',
   styleUrls: ['./usuario-info.page.scss'],
 })
+
+
 export class UsuarioInfoPage implements OnInit {
 
-  constructor() { }
+  userid
+  Users: (IUser)[] = [];
+  nombre
+  telefono
+  preferencias
+  nombreInput
+  telefonoInput
+  preferenciasInput
+  key
+  pwd: string;
+  pwdInput: string;
 
-  ngOnInit() {
+  constructor(private _toastCtrl: ToastController ,private _Libroservice: LibroService,private _activateRoute: ActivatedRoute) 
+  {
+  }
+
+  ngOnInit() 
+  {
+
+    this.userid =this._activateRoute.snapshot.paramMap.get('useridInput');
+    console.log(this.userid)
+    let ref = this._Libroservice.GetUsers();
+    ref.once("value", snapshot => {
+
+      snapshot.forEach(child => {
+        console.log("he encontrado " + child.val().userid);
+        let user: IUser = {
+          "userid": child.val().userid,
+          "pwd": child.val().pwd,
+          "nombre": child.val().nombre,
+          "tlf": child.val().tlf,
+          "preferencias": child.val().preferencias,
+          "key": child.key
+        }
+        this.Users.push(user)
+        console.log(this.Users)
+      })
+
+    }).then(()=>{this.Users.forEach(user=>{
+
+      if(user.userid == this.userid){
+        this.nombre=user.nombre
+        this.preferencias=user.preferencias
+        this.telefono=user.tlf
+        this.key=user.key
+        this.pwd=user.pwd
+
+      }
+    })
+    console.log(this.preferencias)})
+    
   }
 
 }
